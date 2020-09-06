69b0053e340cd9829c1378747234967ac5a8e084
leooliveira-dev <dev@leooliveira.me>
2020-09-06 13:45:02 -0300

    Fix Tailwind import
    Using the ~ as an alias for your src folder doesn't work in this case.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-require("~/tailwind.css");
+require("../tailwind.css");
 
 export default function (Vue, { router, head, isClient }) {
 
