c113141c5b8d1c0aa6672efcceb2b6776cd5e716
Leo Oliveira <dev@leooliveira.me>
2020-09-06 08:59:36 -0300

    That's it.

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+require("~/tailwind.css");
+
+export default function (Vue, { router, head, isClient }) {
+
+}
---
+++ src/pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+	<main>
+		<h1>Blank</h1>
+	</main>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
