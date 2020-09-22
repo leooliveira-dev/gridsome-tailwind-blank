c770ba217c4c04b3706fe11070f1c35d2e7f5097
leooliveira-dev <dev@leooliveira.me>
2020-09-22 15:18:05 -0300

    Added responsive breakpoints and spacings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,24 @@
 module.exports = {
   purge: [],
   theme: {
+    container: {
+      center: true
+      padding: {
+        default: "1rem";
+        phoneL: "2rem";
+        desktop: "0"
+      }
+    }
+    screens: {
+      "phoneS": "320px",
+      "phoneM": "375px",
+      "phoneL": "425px",
+      "phoneXL": "640px",
+      "tablet": "768px",
+      "laptopS": "1024px",
+      "laptopL": "1280px",
+      "desktop": "1440px"
+    },
     extend: {},
   },
   variants: {},
